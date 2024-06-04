f32bf456f7259e5a60f698d2b5aeed68f1c6322b
igor20192 <igor.udovenko2012@meta.ua>
2024-06-04 22:11:47 +0300

    settings.py ALLOWED_HOSTS = ["*"]

--- blogify/settings.py
+++ blogify/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
