3a78baa7fba24546ca65bcf047f04e52dfd5756e
igor20192 <igor.udovenko2012@meta.ua>
2024-06-05 12:40:23 +0300

    edit telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,64 +1,69 @@
 import os
 import logging
-import pdb
-from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, CallbackContext
-from django.conf import settings
+from urllib import response
+import aiohttp
+import asyncio
 import django
-import requests
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from asgiref.sync import sync_to_async
 
+logging.basicConfig(
+    filename="telegram_bot.log", format="[%(asctime)s] [%(levelname)s] => %(message)s]"
+)
+
+# Установите переменную окружения для файла настроек Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogify.settings")
 
+# Инициализируйте Django
 django.setup()
 
-
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-logger = logging.getLogger(__name__)
+# Теперь вы можете импортировать модели и доступ к настройкам
+from blog.models import Article  # Импортируйте ваши модели
+from django.conf import settings
 
 
-# Define the bot commands
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        "Привет! Я ваш блог-бот. Используйте команду /help для получения списка доступных команд."
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Welcome! Use /latest to get the latest article.")
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        "Available commands:\n/start - Welcome message\n/help - List of commands\n/latest - Get the latest article."
     )
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        "Список доступных команд:\n"
-        "/start - приветственное сообщение.\n"
-        "/help - список доступных команд и их описания.\n"
-        "/latest - получить свежую статью из блога."
-    )
+async def get_latest_article():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{settings.API_URL}/api/articles/latest1/") as response:
+            assert response.status == 200
+            return await response.json()
 
 
-def latest(update: Update, context: CallbackContext) -> None:
-    response = requests.get(f"{settings.API_URL}/api/articles/latest/")
-    if response.status_code == 200:
-        article = response.json()
-        message = f"Свежая статья:\n\n{article['title']}\n\n{article['content']}"
+async def latest(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    response = await get_latest_article()
+    if response:
+
+        message = f"Recent article:\n\n{response['title']}\n\n{response['content']}"
+
     else:
         message = "Не удалось получить свежую статью."
-    update.message.reply_text(message)
+    await update.message.reply_text(message)
 
 
-def main() -> None:
-    # Initialize bot and updater
-    updater = Updater(bot=Bot, update_queue=settings.TELEGRAM_TOKEN)
-    dispatcher = updater.dispatcher
+def main():
+    # Создаем объект приложения
+    application = Application.builder().token(settings.TELEGRAM_TOKEN).build()
 
-    # Register command handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("latest", latest))
+    # Добавляем обработчики команд
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("latest", latest))
 
-    # Start the Bot
-    updater.start_polling()
-    updater.idle()
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == "__main__":
+
     main()
