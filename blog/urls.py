e8d5a30677a98ab7019b3465a94707a127f74810
igor20192 <igor.udovenko2012@meta.ua>
2024-06-07 23:11:55 +0300

    blog urls.py add path "articles/latest/"

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ArticleListCreate, ArticleDetail
+from .views import ArticleListCreate, ArticleDetail, LatestArticleView
 
 urlpatterns = [
     path("articles/", ArticleListCreate.as_view(), name="article-list-create"),
     path("articles/<int:pk>/", ArticleDetail.as_view(), name="article-detail"),
+    path("articles/latest/", LatestArticleView.as_view(), name="latest-article"),
 ]
