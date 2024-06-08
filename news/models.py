b351d3d7a633244af935f2c9d460a44d3159c016
igor20192 <igor.udovenko2012@meta.ua>
2024-06-08 14:28:29 +0300

    news models.py add model  NewsArticle

--- news/models.py
+++ news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
