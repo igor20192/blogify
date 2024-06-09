5fdb1e62d6291c3f0f8470af9e9081ab0342a5d1
igor20192 <igor.udovenko2012@meta.ua>
2024-06-09 13:33:31 +0300

    news views.py add class NewsArticleListCreate(generics.ListCreateAPIView):

--- news/views.py
+++ news/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import NewsArticle
+from .serializers import NewsArticleSerializer
 
-# Create your views here.
+
+class NewsArticleListCreate(generics.ListCreateAPIView):
+    """
+    API view to retrieve list of news articles or create new ones.
+
+    This view handles two types of requests:
+
+    1. GET: Returns a list of all news articles in the database.
+    2. POST: Allows the creation of new news articles. The request body should contain
+       the necessary data fields as defined in the `NewsArticleSerializer`.
+
+    Attributes:
+        queryset (QuerySet): The queryset that provides the list of news articles.
+        serializer_class (Serializer): The serializer class used to validate and serialize data.
+
+    Methods:
+        get_queryset(): Returns the queryset of all news articles.
+        get_serializer_class(): Returns the serializer class for the news articles.
+    """
+
+    queryset = NewsArticle.objects.all()
+    serializer_class = NewsArticleSerializer
