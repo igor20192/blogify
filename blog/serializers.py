725d7cf887f3beb976b274ffdf6699762f5d405a
igor20192 <igor.udovenko2012@meta.ua>
2024-06-09 13:25:28 +0300

    blog serializer.py add SubscriberSerializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,5 +1,7 @@
+from pyexpat import model
+from attr import field
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Subscriber
 
 
 class ArticleSerializer(serializers.ModelSerializer):
@@ -28,3 +30,24 @@ class ArticleSerializer(serializers.ModelSerializer):
         - A new Article object."""
         validated_data["author"] = self.context["request"].user
         return super().create(validated_data)
+
+
+class SubscriberSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Subscriber model.
+
+    This serializer is used to convert the Subscriber model instances to and from
+    JSON format, enabling the representation and manipulation of subscriber data
+    through API requests.
+
+    Attributes:
+        Meta (class): Inner class that defines the metadata for the serializer.
+            model (Model): Specifies the model class that the serializer is based on.
+            fields (list[str]): List of model fields that should be included in the
+                                serialized representation. In this case, only the
+                                'chat_id' field is included.
+    """
+
+    class Meta:
+        model = Subscriber
+        fields = ["chat_id"]
