d0849c3964e0e32fb64d01a3dd2bbee8ad3ca137
igor20192 <igor.udovenko2012@meta.ua>
2024-06-09 13:26:25 +0300

    blog urls.py add path "subscribe/"

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ArticleListCreate, ArticleDetail, LatestArticleView
+from .views import (
+    ArticleListCreate,
+    ArticleDetail,
+    LatestArticleView,
+    SubscriberApiView,
+)
 
 urlpatterns = [
     path("articles/", ArticleListCreate.as_view(), name="article-list-create"),
     path("articles/<int:pk>/", ArticleDetail.as_view(), name="article-detail"),
     path("articles/latest/", LatestArticleView.as_view(), name="latest-article"),
+    path("subscribe/", SubscriberApiView.as_view(), name="subscribe"),
 ]
