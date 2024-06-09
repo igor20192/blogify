201bd7ae0b4aec7122ad8951cc86ddeb3031fbfc
igor20192 <igor.udovenko2012@meta.ua>
2024-06-09 15:51:01 +0300

    news srializers.py edit import

--- news/serializers.py
+++ news/serializers.py
@@ -1,10 +1,20 @@
-from xml.parsers.expat import model
-from attr import field
 from rest_framework import serializers
 from .models import NewsArticle
 
 
 class NewsArticleSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for NewsArticle model.
+
+    This serializer handles the serialization and deserialization of NewsArticle objects,
+    converting between model instances and JSON representations. It specifies the fields
+    to be included in the serialized output.
+
+    Attributes:
+        model (type): The model class that this serializer serializes.
+        fields (list): The list of fields to be included in the serialized output.
+    """
+
     class Meta:
         model = NewsArticle
         fields = ["title", "url"]
