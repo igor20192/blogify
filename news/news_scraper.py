8ab747a18525cf045716e24cda9f5387f1cfb998
igor20192 <igor.udovenko2012@meta.ua>
2024-06-09 13:31:21 +0300

    new_scraper.py edit def save_news str 72-86 add api

--- news/news_scraper.py
+++ news/news_scraper.py
@@ -1,8 +1,11 @@
 import os
 import sys
+import aiohttp
+import asyncio
 import django
 import logging
 import requests
+from decouple import config
 from bs4 import BeautifulSoup
 from django.conf import settings
 
@@ -15,8 +18,6 @@ sys.path.append(project_root)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogify.settings")
 django.setup()
 
-# Import model after configuring Django
-from news.models import NewsArticle
 
 # Configure logging
 logging.basicConfig(
@@ -55,12 +56,12 @@ def fetch_news():
     return news_items
 
 
-def save_news(news_items: list[dict]) -> None:
+async def save_news(news_items: list[dict]) -> None:
     """
-    Saves fetched news articles to the database.
+    Saves fetched news articles to the database via Django REST API.
 
     This function iterates through the provided list of news articles (dictionaries)
-    and attempts to create them in the `NewsArticle` model. If an article with the
+    and attempts to create them via the `NewsArticle` API endpoint. If an article with the
     same title and URL already exists, it is skipped to avoid duplicates. Any exceptions
     encountered during saving are logged for debugging purposes.
 
@@ -68,17 +69,27 @@ def save_news(news_items: list[dict]) -> None:
         news_items (list[dict]): A list of dictionaries containing news article information
                                 (title and URL).
     """
-    for item in news_items:
-        try:
-            NewsArticle.objects.get_or_create(title=item["title"], url=item["url"])
-        except Exception as e:
-            logging.error(f"Error saving article {item['title']}: {e}")
+    api_url = f"{config('API_URL')}/api/news/"
+    async with aiohttp.ClientSession() as session:
+        for item in news_items:
+            try:
+                async with session.post(api_url, json=item) as response:
+                    if response.status == 201:
+                        logging.info(f"Successfully saved article: {item['title']}")
+                    elif response.status == 400:
+                        logging.warning(f"Article already exists: {item['title']}")
+                    else:
+                        logging.exception(
+                            f"Failed to save article {item['title']}: {response.status}"
+                        )
+            except Exception as e:
+                logging.error(f"Error saving article {item['title']}: {e}")
 
 
 if __name__ == "__main__":
     news_items = fetch_news()
     if news_items:
-        save_news(news_items)
+        asyncio.run(save_news(news_items))
         logging.info("News articles fetched and saved successfully.")
     else:
         logging.info("No news articles fetched.")
